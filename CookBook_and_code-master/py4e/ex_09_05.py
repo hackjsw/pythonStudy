f00d02a2196d2676665571061e0281838e17ff4a
hackjsw <110555343@qq.com>
2019-03-14 16:32:09 +0800

    new py4e

---
+++ CookBook_and_code-master/py4e/ex_09_05.py
@@ -0,0 +1,22 @@
+ffile = input('Input File:')
+files = open(ffile)
+#取关键字-邮件地址
+a = []
+for line in files:
+    words = line.split()
+    #print(words)
+    if len(words) > 3 and words[0] == 'From':
+        a.append(words[1])
+#字符分段，取邮件地址
+shoolCount = []
+for word in a:
+    b = word.split('@')
+#    print(b[1])
+    shoolCount.append(b[1])
+#字典值加权
+fb = {}
+for x in shoolCount:
+    fb[x] = fb.get(x, 0) + 1
+
+print(fb)
+
